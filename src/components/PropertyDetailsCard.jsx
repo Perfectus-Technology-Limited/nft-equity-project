5558d34a78a22128f8f222506ba1a400bbcdce5a
Kavindu Madushanka <kavindumadushanka972@gmail.com>
2023-06-19 10:09:47 +0530

    property detail card component

---
+++ src/components/PropertyDetailsCard.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { Card, Typography } from 'antd';
+import { Row, Col } from 'reactstrap';
+
+const PropertyDetailsCard = () => {
+  const { Title, Text } = Typography;
+
+  return (
+    <Card className="nft-square-card">
+      <Row>
+        <Col xxl="7">
+          <Title level={5}>
+            <span className="text-primary">BERAWA PROPERTY</span>
+            <span>{` `}DETAILS</span>
+          </Title>
+
+          <div className="mt-3">
+            <Text type="secondary" className="property-description">
+              In the heart of the bustling and exploding Canggu of Bali,
+              Indonesia. Property prices are set to explode over the next decade
+              so why not take advantage today and own a piece of the island of
+              paradise and earn some passive income along the way.
+            </Text>
+          </div>
+
+          <Row className="mt-4">
+            <Text className="options-section">&#x2022; 3 Bedroom Villa</Text>
+            <Text className="options-section">&#x2022; 220m2 Villa</Text>
+            <Text className="options-section">&#x2022; 190m2 Plot</Text>
+            <Text className="options-section">&#x2022; Leasehold</Text>
+          </Row>
+        </Col>
+
+        <Col>
+          <img
+            src="https://images.pexels.com/photos/280222/pexels-photo-280222.jpeg?cs=srgb&dl=pexels-pixabay-280222.jpg&fm=jpg"
+            className="property-image"
+            alt="property"
+          />
+          <div className="text-center view-full-property-bg">
+            <Text className="small text-light">VIEW FULL PROPERTY</Text>
+          </div>
+        </Col>
+      </Row>
+
+      <Row className="mt-4 col-10 options-section">
+        <Col xxl="4" xl="4" lg="4" md="4" sm="4" xs="4">
+          <Text className="text-primary">&#x2022; 3 Parking/Garage</Text>
+        </Col>
+        <Col xxl="4" xl="4" lg="4" md="4" sm="4" xs="4">
+          <Text className="text-primary">&#x2022; Porter/Security</Text>
+        </Col>
+        <Col xxl="4" xl="4" lg="4" md="4" sm="4" xs="4">
+          <Text className="text-primary">&#x2022; Air Conditioning</Text>
+        </Col>
+
+        <Col xxl="4" xl="4" lg="4" md="4" sm="4" xs="4">
+          <Text className="text-primary">&#x2022; Washer</Text>
+        </Col>
+        <Col xxl="4" xl="4" lg="4" md="4" sm="4" xs="4">
+          <Text className="text-primary">&#x2022; Television</Text>
+        </Col>
+        <Col xxl="4" xl="4" lg="4" md="4" sm="4" xs="4">
+          <Text className="text-primary">&#x2022; Balcony/Terrace</Text>
+        </Col>
+
+        <Col xxl="4" xl="4" lg="4" md="4" sm="4" xs="4">
+          <Text className="text-primary">&#x2022; Onsite Maintenance</Text>
+        </Col>
+        <Col xxl="4" xl="4" lg="4" md="4" sm="4" xs="4">
+          <Text className="text-primary">&#x2022; Barbeque</Text>
+        </Col>
+        <Col xxl="4" xl="4" lg="4" md="4" sm="4" xs="4">
+          <Text className="text-primary">&#x2022; Ventilation</Text>
+        </Col>
+
+        <Col xxl="4" xl="4" lg="4" md="4" sm="4" xs="4">
+          <Text className="text-primary">&#x2022; Outdoor Shower</Text>
+        </Col>
+        <Col xxl="4" xl="4" lg="4" md="4" sm="4" xs="4">
+          <Text className="text-primary">&#x2022; Garden</Text>
+        </Col>
+        <Col xxl="4" xl="4" lg="4" md="4" sm="4" xs="4">
+          <Text className="text-primary">&#x2022; Rural/Secluded</Text>
+        </Col>
+
+        <Col xxl="4" xl="4" lg="4" md="4" sm="4" xs="4">
+          <Text className="text-primary">&#x2022; Microwave</Text>
+        </Col>
+        <Col xxl="4" xl="4" lg="4" md="4" sm="4" xs="4">
+          <Text className="text-primary">&#x2022; Intercom</Text>
+        </Col>
+        <Col xxl="4" xl="4" lg="4" md="4" sm="4" xs="4">
+          <Text className="text-primary">&#x2022; Parking</Text>
+        </Col>
+      </Row>
+    </Card>
+  );
+};
+
+export default PropertyDetailsCard;
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { Typography } from 'antd';
+import PropertyDetailsCard from '@/components/PropertyDetailsCard';
+import { Col, Row } from 'reactstrap';
 
 const HomePage = () => {
 
-  const { Title } = Typography;
-
   return (
-    <div>
-      <Title>Home</Title>
+    <div className='mt-5'>
+      <Row>
+        <Col xxl="8" xl="8" lg="8" md="8" sm="12" xs="12">
+          <PropertyDetailsCard />
+        </Col>
+
+        <Col xxl="4" xl="4" lg="4" md="4" sm="12" xs="12">
+          
+        </Col>
+      </Row>
     </div>
   )
 }
