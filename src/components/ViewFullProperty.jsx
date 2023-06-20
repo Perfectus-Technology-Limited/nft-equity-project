170a2bc0faea3d43e9a2136bfc735c38b5b1ab8b
Kavindu Madushanka <kavindumadushanka972@gmail.com>
2023-06-20 12:59:36 +0530

    property images carousel

--- src/components/ViewFullProperty.jsx
+++ src/components/ViewFullProperty.jsx
@@ -1,8 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import { Modal, Carousel } from 'antd';
+import { Modal } from 'antd';
 import { useSelector, useDispatch } from 'react-redux';
 import { closeModal } from '@/redux/viewFullPropertySlice';
 import Image from 'next/image';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+
+const getConfigurableProps = () => ({
+  showArrows: boolean('showArrows', true, tooglesGroupId),
+  showStatus: boolean('showStatus', true, tooglesGroupId),
+  showIndicators: boolean('showIndicators', true, tooglesGroupId),
+  infiniteLoop: boolean('infiniteLoop', true, tooglesGroupId),
+  showThumbs: boolean('showThumbs', true, tooglesGroupId),
+  useKeyboardArrows: boolean('useKeyboardArrows', true, tooglesGroupId),
+  autoPlay: boolean('autoPlay', true, tooglesGroupId),
+  stopOnHover: boolean('stopOnHover', true, tooglesGroupId),
+  swipeable: boolean('swipeable', true, tooglesGroupId),
+  dynamicHeight: boolean('dynamicHeight', true, tooglesGroupId),
+  emulateTouch: boolean('emulateTouch', true, tooglesGroupId),
+  autoFocus: boolean('autoFocus', false, tooglesGroupId),
+  thumbWidth: number('thumbWidth', 100, {}, valuesGroupId),
+  selectedItem: number('selectedItem', 0, {}, valuesGroupId),
+  interval: number('interval', 2000, {}, valuesGroupId),
+  transitionTime: number('transitionTime', 500, {}, valuesGroupId),
+  swipeScrollTolerance: number('swipeScrollTolerance', 5, {}, valuesGroupId),
+  ariaLabel: text('ariaLabel', undefined),
+});
+
 
 const ViewFullProperty = () => {
   const dispatch = useDispatch();
@@ -39,10 +63,10 @@ const ViewFullProperty = () => {
       footer={false}
       width={'100vw'}
     >
-      <Carousel autoplay>
+      <Carousel dynamicHeight={true} autoPlay={true} showThumbs={true}>
         {importedImages.map((image, index) => (
           <div>
-            <Image key={index} src={image} alt={`Image ${index + 1}`} />
+            <Image key={index} src={image} alt={`Image ${index + 1}`} className="property-image" />
           </div>
         ))}
       </Carousel>
