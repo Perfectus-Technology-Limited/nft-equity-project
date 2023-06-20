e0a6c0f0da323a989cbdcbb39ddd4248ff9da3d7
Kavindu Madushanka <kavindumadushanka972@gmail.com>
2023-06-20 11:09:47 +0530

    referral link card

---
+++ src/components/ReferralLink.jsx
@@ -0,0 +1,67 @@
+import { Card, Typography, Space, Input, Button, Tooltip } from 'antd';
+import React, { useState } from 'react';
+import { CopyFilled, CheckOutlined } from '@ant-design/icons';
+
+const ReferralLink = () => {
+  const { Title, Text } = Typography;
+  const [isRefLinkCopied, setIsRefLinkCopied] = useState(false);
+  const [refLink, setRefLink] = useState('https://nftequity.group/?ref=0xC634567890abcdef9AC0d715BD91b3083C71193f')
+
+  const handleCopy = () => {
+    setIsRefLinkCopied(true);
+    navigator.clipboard.writeText(refLink)
+    setTimeout(() => {
+      setIsRefLinkCopied(false);
+    }, 2000);
+  };
+
+  return (
+    <Card className="nft-square-card nft-dark-card">
+      <div className="text-center">
+        <Title level={5}>
+          <span className="text-primary">REFERRAL</span>
+          <span>{` `}LINK</span>
+        </Title>
+
+        <div className="mt-3">
+          <Text type="secondary" className="text-uppercase">
+            Receive a 10% share when SOMEONE utilize your referral link.
+          </Text>
+        </div>
+
+        <div className="col-lg-10 mx-auto mt-2">
+          <Space.Compact
+            style={{
+              width: '100%',
+            }}
+          >
+            <Input
+              value={refLink}
+              className="text-center"
+            />
+
+            {!isRefLinkCopied ? (
+              <Tooltip title="Copy">
+                <Button type="primary" onClick={handleCopy}>
+                  <div style={{ marginTop: '0px' }}>
+                    <CopyFilled />
+                  </div>
+                </Button>
+              </Tooltip>
+            ) : (
+              <Tooltip title="Copied">
+                <Button type="primary">
+                  <div style={{ marginTop: '0px' }}>
+                    <CheckOutlined />
+                  </div>
+                </Button>
+              </Tooltip>
+            )}
+          </Space.Compact>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default ReferralLink;
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -3,6 +3,7 @@ import PropertyDetailsCard from '@/components/PropertyDetailsCard';
 import RentalRevenueCal from '@/components/RentalRevenueCal';
 import NftCard from '@/components/NftCard';
 import { Col, Row } from 'reactstrap';
+import ReferralLink from '@/components/ReferralLink';
 
 const HomePage = () => {
   const sampleNftData = [
@@ -16,7 +17,7 @@ const HomePage = () => {
       price: 4700,
       sharedRevenue: '15%',
       APR: '≤ 8%',
-      equityShare: '0.85%/NFT'
+      equityShare: '0.85%/NFT',
     },
     {
       id: 2,
@@ -28,7 +29,7 @@ const HomePage = () => {
       price: 6500,
       sharedRevenue: '22.5%',
       APR: '≤ 10%',
-      equityShare: '0.85%/NFT'
+      equityShare: '0.85%/NFT',
     },
     {
       id: 3,
@@ -40,7 +41,7 @@ const HomePage = () => {
       price: 8400,
       sharedRevenue: '27.5%',
       APR: '≤ 12%',
-      equityShare: '1.16%/NFT'
+      equityShare: '1.16%/NFT',
     },
     {
       id: 4,
@@ -52,28 +53,43 @@ const HomePage = () => {
       price: 12500,
       sharedRevenue: '35%',
       APR: '≤ 15%',
-      equityShare: '2%/NFT'
+      equityShare: '2%/NFT',
     },
   ];
 
   return (
     <div className="mt-5 mb-5">
       <Row>
-        <Col xxl="8" xl="8" lg="8" md="8" sm="12" xs="12" className='mt-3'>
+        <Col xxl="8" xl="8" lg="8" md="8" sm="12" xs="12" className="mt-3">
           <PropertyDetailsCard />
         </Col>
 
-        <Col xxl="4" xl="4" lg="4" md="4" sm="12" xs="12" className='mt-3'>
+        <Col xxl="4" xl="4" lg="4" md="4" sm="12" xs="12" className="mt-3">
           <RentalRevenueCal />
         </Col>
       </Row>
 
       <Row className="mt-5">
         {sampleNftData.map((nft) => (
-          <Col xxl="3" xl="3" lg="3" md="6" sm="12" xs="12" className='mt-3' key={nft.id}>
+          <Col
+            xxl="3"
+            xl="3"
+            lg="3"
+            md="6"
+            sm="12"
+            xs="12"
+            className="mt-3"
+            key={nft.id}
+          >
             <NftCard nftData={nft} />
           </Col>
         ))}
+      </Row>
+
+      <Row className="mt-5">
+        <Col>
+          <ReferralLink />
+        </Col>
       </Row>
     </div>
   );
